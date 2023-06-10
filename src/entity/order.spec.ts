96fc1ce7a179bb58951e1008bfb954eb2577a6bb
rafael <rbtravassos@gmail.com>
2023-06-09 22:24:41 -0300

    Criado os testes de Order

---
+++ src/entity/order.spec.ts
@@ -0,0 +1,36 @@
+import Order from "./order";
+import OrderItem from "./order_item";
+
+describe("Order unit tests", () => {
+
+    it("should throw error when id is empty", () => {
+        expect(() => {
+            let order = new Order("", "123", []);
+        }
+        ).toThrowError("Id is required")
+    });
+
+    it("should throw error when Customer id is empty", () => {
+        expect(() => {
+            let order = new Order("123", "", []);
+        }
+        ).toThrowError("CustomerId is required")
+    });
+
+    it("should throw error when items is empty", () => {
+        expect(() => {
+            let order = new Order("123", "123", []);
+        }
+        ).toThrowError("item qtd must be greater than zero")
+    });
+
+    it("should calculate total", () => {
+        const item = new OrderItem("i1", "item 1", 100);
+        const order = new Order("123","123", [item]);
+        expect(order.total()).toBe(100);
+
+        const item2 = new OrderItem("i2", "item 2", 200);
+        const order2 = new Order("123", "123", [item, item2]);
+        expect(order2.total()).toBe(300);
+    })
+})
--- src/entity/order.ts
+++ src/entity/order.ts
@@ -1,19 +1,31 @@
 import OrderItem from "./order_item";
 export default class Order {
-    _id: string;
-    _customerId: string;
-    _items: OrderItem[] = [];
-    _total: number;
+    private _id: string;
+    private _customerId: string;
+    private _items: OrderItem[] = [];
+    private _total: number;
 
     constructor(id: string, customerId:string, items: OrderItem[]){
         this._id = id;
         this._customerId=customerId;
         this._items = items;
         this._total = this.total();
+        this.validate();
+    }
+    validate(): boolean {
+        if (this._id.length==0) {
+            throw("Id is required");
+        }
+        if (this._customerId.length==0) {
+            throw("CustomerId is required");
+        }
+        if (this._items.length==0) {
+            throw("item qtd must be greater than zero")
+        }
+        return true;
     }
 
     total(): number {
-        return 0;
-        // return this._items.reduce((acc, item) => acc + item.price, 0);
+        return this._items.reduce((acc, item) => acc + item.price, 0);
     }
 }
