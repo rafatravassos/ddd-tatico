3d7342914538cd293f57614f1bf65bcc63d13a2c
rafael travassos <rbtravassos@gmail.com>
2024-07-18 09:07:48 -0300

    Adicionado o validador para products e customer utilizando o YUP

--- src/domain/customer/entity/customer.ts
+++ src/domain/customer/entity/customer.ts
@@ -1,5 +1,6 @@
 import Entity from "../../@shared/entity/entity.abstract";
 import NotificationError from "../../@shared/notification/notification.error";
+import CustomerValidatorFactory from "../factory/customer.validator.factory";
 import Address from "../value-object/address";
 import { CustomerInterface } from "./customer.interface";
 
@@ -20,19 +21,9 @@ export default class Customer extends Entity implements CustomerInterface{
         }
     }
 
-    validate(){
-        if (this._id.length==0){
-            this.notification.addError({
-                context: "customer",
-                message: "The ID is required."
-            });
-        }
-        if (this._name.length==0){
-            this.notification.addError({
-                context: "customer",
-                message: "The name is required."
-            });
-        }
+    validate()
+    {
+        CustomerValidatorFactory.create().validate(this);
     }
 
     get Address() {
